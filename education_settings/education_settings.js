795c8cb336b9a29055a393593aa32118a6e13028
baha eddine weslati <54307698+bahaou@users.noreply.github.com>
2021-09-24 16:53:51 +0100

    Add files via upload

---
+++ education_settings/education_settings.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Education Settings', {
+	refresh: function(frm) {
+
+	},
+	
+	
+	
+	
+	print_bulletins: function(frm) {
+		
+			window.open('/printbulletin');
+
+		},
+		
+	management: function(frm) {
+		
+			window.open('/management');
+
+		}
+	
+	
+	
+	
+	
+});
+
+
+	
